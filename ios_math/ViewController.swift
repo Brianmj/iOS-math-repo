324972d08f3eda7bb579c8a3d380ea8f4c7ec6df
Brian Michael Jones <spamthiseml@hotmail.com>
2016-06-15 16:50:12 -0400

    Initial Commit

---
+++ ios_math/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_math
+//
+//  Created by Brian Jones on 6/15/16.
+//  Copyright © 2016 Brian Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
